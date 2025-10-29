524630901177c78338e8afd94deb538bec1265f9
Animesh-108 <dey367846@gmail.com>
2025-10-29 22:21:21 +0600

    Update variables.tf with new EKS configurations
    Added new variables for instance types and pipeline role ARNs.

--- variables.tf
+++ variables.tf
@@ -1,13 +1,37 @@
 variable "region" {
-  description = "AWS region for the cluster"
+  description = "AWS region"
   type        = string
   default     = "us-east-1"
 }
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
+
+variable "cluster_name_prefix" {
+  description = "Prefix for the EKS cluster name"
+  type        = string
+  default     = "my-app"
+}
+
+variable "non_prod_instance_type" {
+  description = "Instance type for non-prod nodes"
   type        = string
 }
-variable "instance_type" {
-  description = "EC2 instance type for worker nodes"
+
+variable "prod_instance_type" {
+  description = "Instance type for prod nodes"
+  type        = string
+}
+
+variable "node_group_max_size" {
+  description = "Initial max size for node group"
+  type        = number
+  default     = 1 # Start with 1 to deploy quickly
+}
+
+variable "non_prod_pipeline_role_arn" {
+  description = "ARN of the non-prod pipeline IAM role (Create in Phase 5)"
+  type        = string
+}
+
+variable "prod_pipeline_role_arn" {
+  description = "ARN of the prod pipeline IAM role (Create in Phase 5)"
   type        = string
 }
